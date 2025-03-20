6d155ac8ba792103b826637ffa006b37413d126c
divyanshMauryaaa <divyanshm510@gmail.com>
2025-03-20 22:00:08 +0530

    Change export to const for supabase client initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import ReactMarkdown from "react-markdown";
 import { useUser } from "@clerk/nextjs";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL || '',
   process.env.NEXT_PUBLIC_SUPABASE_PROJECT_APIKEY || ''
 );
