860577abc8d279f44553f826f1b05dfcf4129ad4
divyanshMauryaaa <divyanshm510@gmail.com>
2025-04-10 13:16:09 +0530

    fixed pageprops error

--- src/app/(workspaces)/workspaces/[workspaceID]/page.tsx
+++ src/app/(workspaces)/workspaces/[workspaceID]/page.tsx
@@ -1,26 +1,23 @@
-// app/workspaces/[workspaceId]/page.tsx
-import { createClient } from "@supabase/supabase-js";
-import WorkspaceClient from "./workspaceclient";
+import WorkspaceClient from './workspaceclient';
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL || "",
-  process.env.NEXT_PUBLIC_SUPABASE_PROJECT_APIKEY || ""
+  process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL || '',
+  process.env.NEXT_PUBLIC_SUPABASE_PROJECT_APIKEY || ''
 );
 
-export default async function WorkspacePage({
-  params,
-}: {
-  params: { workspaceID: string };
-}) {
+type PageProps = {
+  params: {
+    workspaceID: string;
+  };
+};
+
+export default async function WorkspacePage({ params }: PageProps) {
   const { data, error } = await supabase
-    .from("workspaces")
-    .select("*")
-    .eq("id", params.workspaceID)
+    .from('workspaces')
+    .select('*')
+    .eq('id', params.workspaceID)
     .single();
 
-  if (error) {
-    return <div>Error fetching workspace: {error.message}</div>;
-  }
-
-  return <WorkspaceClient workspace={data} />;
+  return <WorkspaceClient workspace={data} error={error?.message} />;
 }
